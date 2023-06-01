486187466504266d441062c963faa2ebfb1fa981
dammarts <dammarts01@gmail.com>
2023-06-01 16:40:04 -0500

    cambios de qa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { QaComponent } from './qa/qa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    PruebaComponent
+    PruebaComponent,
+    QaComponent
   ],
   imports: [
     BrowserModule
