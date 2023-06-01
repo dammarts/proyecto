3c5940ea14dd44a1f5bd83c58e24bdef80d88397
dammarts <dammarts01@gmail.com>
2023-06-01 16:47:05 -0500

    Revert "cambio en dev"
    This reverts commit 0aefa84a6b513449ace1c79499108a64e74004b7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { PruebaComponent } from './prueba/prueba.component';
-import { DevComponent } from './dev/dev.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    PruebaComponent,
-    DevComponent
+    PruebaComponent
   ],
   imports: [
     BrowserModule
