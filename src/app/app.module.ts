0aefa84a6b513449ace1c79499108a64e74004b7
dammarts <dammarts01@gmail.com>
2023-06-01 16:31:36 -0500

    cambio en dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { DevComponent } from './dev/dev.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    PruebaComponent
+    PruebaComponent,
+    DevComponent
   ],
   imports: [
     BrowserModule
