708db3855354ba1d06b87e29110bdd2326e899ef
dammarts <dammarts01@gmail.com>
2023-06-13 18:06:55 -0500

    paso de qa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { PruebaComponent } from './prueba/prueba.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    PruebaComponent
+   
   ],
   imports: [
     BrowserModule
