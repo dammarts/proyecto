0b585ac15b6667e4f259c1e085d8f575d6755e3c
dammarts <dammarts01@gmail.com>
2023-06-13 17:59:29 -0500

    paguina principal lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AllComponent } from './all/all.component';
-import { PruebasComponent } from './pruebas/pruebas.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import {CdkAccordionModule} from '@angular/cdk/accordion';
 import { FooterComponent } from './footer/footer.component';
@@ -16,8 +14,6 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    AllComponent,
-    PruebasComponent,
     AccordionComponent,
     FooterComponent,
     
